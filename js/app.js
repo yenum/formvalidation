2604ca284919a3548b3d66b566dfa4f264c2b8b8
Yenum <87123146+yenum@users.noreply.github.com>
2021-09-14 19:29:14 +0100

    First Commit

---
+++ js/app.js
@@ -0,0 +1,6 @@
+const usernameEl = document.querySelector('#username');
+const emailEl = document.querySelector('#email');
+const passwordEl = document.querySelector('#password');
+const confirmPasswordEl = document.querySelector('#confirm-password');
+
+const form = document.querySelector('#signup');
